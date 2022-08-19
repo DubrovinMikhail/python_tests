8f11d6363ad3e3b358095cf0fe6e2c495591de81
Mikhail Dubrovin <78617456+DubrovinMikhail@users.noreply.github.com>
2022-08-19 14:24:36 +0300

    Update and rename test_pytest.py to accounting_pytest.py

--- tests/accounting_pytest.py
+++ tests/accounting_pytest.py
@@ -1,6 +1,5 @@
 import os
-import main
-import yadisk
+import accounting
 import requests
 from accounting import check_document_existance, get_doc_owner_name, \
     get_all_doc_owners_names, remove_doc_from_shelf, add_new_shelf, \
@@ -53,26 +52,6 @@ class TestFunc:
         assert doc in main.documents and "2002 543821" in main.directories["3"]
 
 
-class Test_Uplouder:
-    token = yadisk.YaUploader.token
-    host = yadisk.YaUploader.host
-    url = f'{host}/v1/disk/resources'
-    def test_response(self):
-        params = {'path': "/"}
-        headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {Test_Uplouder.token}'}
-        response = requests.get(Test_Uplouder.url, params=params, headers=headers).status_code
-        assert response == 200
-
-    def test_uploader(self):
-        href = "https://cloud-api.yandex.net:443"
-        name_dir = "test_dir"
-        uploader = yadisk.YaUploader(os.getenv("TOKEN"))
-        uploader.dir_inst(name_dir)
-        url = f'{yadisk.YaUploader.host}/v1/disk/resources'
-        params = {'path': '/'}
-        response = requests.get(url, params=params, headers=yadisk.YaUploader.headers).json()
-        name_list = [a["name"] for a in [items for items in response["_embedded"]["items"]]]
-        assert name_dir in name_list
 
 
 
